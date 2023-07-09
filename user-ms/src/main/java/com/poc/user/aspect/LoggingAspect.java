206d7fa67dc94217f33c80281fca725acd784b12
George Sorotos <george.sorotos@vodafone.com>
2023-07-09 19:00:43 +0300

    fix in order aspect and @Transactional to coexist

--- user-ms/src/main/java/com/poc/user/aspect/LoggingAspect.java
+++ user-ms/src/main/java/com/poc/user/aspect/LoggingAspect.java
@@ -1,33 +1,31 @@
 package com.poc.user.aspect;
 
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /**
- * Aspect for logging execution of service, DAO, 
+ * Aspect for logging execution of service, DAO,
  * repository and controller methods and exceptions
  *
  * @author didel
- *
  */
 @Aspect
 @Component
 @Log4j2
 public class LoggingAspect {
-    @Before("execution(* com.poc.user.service.*.*(..))")
-    public void interceptMethodsBeforeCall(JoinPoint joinPoint) {
+    @Around("execution(* com.poc.user.service.*.*(..))")
+    @SneakyThrows
+    public Object interceptAroundMethods(ProceedingJoinPoint joinPoint) {
         log.info("Intercepted method: {}, called", () -> joinPoint.getSignature().toShortString());
-    }
-
-    @After("execution(* com.poc.user.service.*.*(..))")
-    public void interceptMethodsAfterCall(JoinPoint joinPoint) {
+        Object proceed = joinPoint.proceed();
         log.info("Intercepted method: {} end", () -> joinPoint.getSignature().toShortString());
+        return proceed;
     }
 
     @AfterThrowing(pointcut = "execution(* com.poc.user.service.*.*(..))", throwing = "e")
