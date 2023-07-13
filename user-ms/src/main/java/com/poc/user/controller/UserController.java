e47fc3a024bf05d1e1fa0993a84c39d4efe2a7e0
George Sorotos <george.sorotos@vodafone.com>
2023-07-13 11:17:50 +0300

    controller improvements

--- user-ms/src/main/java/com/poc/user/controller/UserController.java
+++ user-ms/src/main/java/com/poc/user/controller/UserController.java
@@ -20,59 +20,59 @@ import java.util.List;
 
 /**
  * The REST controller implements the REST API for user related and CRUD operations
- * @author didel
  *
+ * @author didel
  */
 @RestController
 @RequestMapping("/v1/users")
 @Tag(name = "User Resource CRUD REST API", description = "CRUD REST APIs - Create UserResponse, " +
-		"Update User(s), Get User(s), (soft) Delete User(s)")
+        "Update User(s), Get User(s), (soft) Delete User(s)")
 @Slf4j
 public class UserController {
 
-		@Autowired
-		private UserService userService;
+    @Autowired
+    private UserService userService;
 
-		@ResponseStatus(HttpStatus.CREATED)
-	    @RequestMapping(method = RequestMethod.POST)
-		@OpenApiCreateUser
-		public Mono<UserResponse> createUser(@Valid @RequestBody UserInfo user) {
-			return userService.createUser(user);
-	    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    @OpenApiCreateUser
+    public Mono<UserResponse> createUser(@Valid @RequestBody UserInfo user) {
+        return userService.createUser(user);
+    }
 
-	    @ResponseStatus(HttpStatus.OK)
-		@GetMapping("/{id}")
-		@OpenApiReadUser
-	    public Mono<UserResponse> readUser(@PathVariable String id) {
-			return userService.readUser(id);
-	    }
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    @OpenApiReadUser
+    public Mono<UserResponse> readUser(@PathVariable String id) {
+        return userService.readUser(id);
+    }
 
-		@ResponseStatus(HttpStatus.OK)
-		@GetMapping()
-		@OpenApiReadUsers
-		public Flux<UserResponse> readUsers(@RequestParam(name="id") List<String> ids) {
-			return userService.readUsers(ids);
-		}
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping()
+    @OpenApiReadUsers
+    public Flux<UserResponse> readUsers(@RequestParam(name = "id") List<String> ids) {
+        return userService.readUsers(ids);
+    }
 
-		@ResponseStatus(HttpStatus.OK)
-		@DeleteMapping("/{id}")
-		@OpenApiDeleteUser
-		public Mono<UserResponse> deleteUser(@PathVariable String id) {
-			return userService.deleteUser(id);
-		}
+    @ResponseStatus(HttpStatus.OK)
+    @DeleteMapping("/{id}")
+    @OpenApiDeleteUser
+    public Mono<UserResponse> deleteUser(@PathVariable String id) {
+        return userService.deleteUser(id);
+    }
 
-	@ResponseStatus(HttpStatus.OK)
-	@PutMapping("/{id}")
-	@OpenApiEditUser
-	public Mono<UserResponse> upsertUser(@PathVariable String id, @Valid @RequestBody UserInfo user) {
-		return userService.upsertUser(id, user);
-	}
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    @OpenApiEditUser
+    public Mono<UserResponse> upsertUser(@PathVariable String id, @Valid @RequestBody UserInfo user) {
+        return userService.upsertUser(id, user);
+    }
 
-	@ResponseStatus(HttpStatus.OK)
-	@PutMapping()
-	@OpenApiEditUsers
-	public Flux<UserResponse> upsertUsers(@RequestBody @NotEmpty(message = "User info list cannot be empty.")
-										  @Valid UserInfoList<ParticularUserInfo> users) {
-		return userService.upsertUsers(users);
-	}
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping()
+    @OpenApiEditUsers
+    public Flux<UserResponse> upsertUsers(@RequestBody @NotEmpty(message = "User info list cannot be empty.")
+                                          @Valid UserInfoList<ParticularUserInfo> users) {
+        return userService.upsertUsers(users);
+    }
 }
